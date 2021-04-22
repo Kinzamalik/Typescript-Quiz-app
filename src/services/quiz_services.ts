1d102f2d4d7d41449e2eff2ab2b74b668bf6ab4a
Kinzamalik <kiinza.malik@gmail.com>
2021-04-22 23:44:45 +0500

    fetch api

--- src/services/quiz_services.ts
+++ src/services/quiz_services.ts
@@ -1,3 +1,7 @@
-function getQuizDetails(){
-    
+
+export const getQuizDetails = async(totalQuestions:number,level:string)=>{
+    const res = await fetch(`https://opentdb.com/api.php?amount=${totalQuestions}&category=9&difficulty=${level}&type=multiple`)
+    let {results} = await res.json();
+
+    return results;
 }
